5acaff7f90ba560af69ddb54c8f584fc8ac488a2
themotionjournal <themotionjournal@gmail.com>
2025-08-20 16:12:18 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,21 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
 import SmoothScroll from '@/components/SmoothScroll'
+import PageTransition from '@/components/PageTransition'
+import MouseSpotlight from '@/components/MouseSpotlight'
 
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-})
+const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <Head><meta name="viewport" content="width=device-width, initial-scale=1" /></Head>
       <div className={`${inter.variable} font-sans`}>
         <SmoothScroll />
-        <Component {...pageProps} />
+        <MouseSpotlight />
+        <PageTransition>
+          <Component {...pageProps} />
+        </PageTransition>
       </div>
     </>
   )
