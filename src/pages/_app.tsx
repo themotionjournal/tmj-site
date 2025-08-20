a4786efab68b722a03b832798afca002b3dcf65e
themotionjournal <themotionjournal@gmail.com>
2025-08-20 15:29:54 +0200

    temp: remove SmoothScroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import SmoothScroll from '../components/SmoothScroll'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <SmoothScroll />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
