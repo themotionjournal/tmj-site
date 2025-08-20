cb1559614e2e609bba454109219a2adea9885da5
themotionjournal <themotionjournal@gmail.com>
2025-08-20 15:22:01 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import SmoothScroll from '@/components/SmoothScroll'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <SmoothScroll />
+      <Component {...pageProps} />
+    </>
+  )
 }
