8703eacf9c030da2e30584cafe095bc34a0ab58e
themotionjournal <themotionjournal@gmail.com>
2025-08-20 15:59:24 +0200

    Update SmoothScroll.tsx

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,1 +1,26 @@
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
 
+export default function SmoothScroll() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smoothWheel: true,
+      lerp: 0.12, // più basso = più “burroso”
+      wheelMultiplier: 1.0,
+    })
+
+    let raf = 0
+    const tick = (time: number) => {
+      lenis.raf(time)
+      raf = requestAnimationFrame(tick)
+    }
+    raf = requestAnimationFrame(tick)
+
+    return () => {
+      cancelAnimationFrame(raf)
+      lenis.destroy()
+    }
+  }, [])
+
+  return null
+}
